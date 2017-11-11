36198ad32d33373858af5769cb4da9b5af222b85
sorainnosia <sorainnosia@gmail.com>
2017-11-11 22:44:11 +0800

    First Release
    a tool to combine lines of text into column of texts

---
+++ Combiner/Form1.Designer.cs
@@ -0,0 +1,137 @@
+﻿namespace Combiner
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.button1 = new System.Windows.Forms.Button();
+            this.txtItemPerRow = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtRaw = new System.Windows.Forms.RichTextBox();
+            this.txtOutput = new System.Windows.Forms.RichTextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtSeparator = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(15, 163);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Combine";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // txtItemPerRow
+            // 
+            this.txtItemPerRow.Location = new System.Drawing.Point(89, 12);
+            this.txtItemPerRow.Name = "txtItemPerRow";
+            this.txtItemPerRow.Size = new System.Drawing.Size(110, 20);
+            this.txtItemPerRow.TabIndex = 1;
+            this.txtItemPerRow.Text = "10";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 15);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(71, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Item Per Row";
+            // 
+            // txtRaw
+            // 
+            this.txtRaw.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtRaw.Location = new System.Drawing.Point(15, 38);
+            this.txtRaw.Name = "txtRaw";
+            this.txtRaw.Size = new System.Drawing.Size(562, 119);
+            this.txtRaw.TabIndex = 3;
+            this.txtRaw.Text = "1\n2\n3\n4\n5\n6\n7\n8\n9\n10\n";
+            // 
+            // txtOutput
+            // 
+            this.txtOutput.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtOutput.Location = new System.Drawing.Point(15, 192);
+            this.txtOutput.Name = "txtOutput";
+            this.txtOutput.Size = new System.Drawing.Size(562, 138);
+            this.txtOutput.TabIndex = 4;
+            this.txtOutput.Text = "";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(222, 15);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(53, 13);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Separator";
+            // 
+            // txtSeparator
+            // 
+            this.txtSeparator.Location = new System.Drawing.Point(299, 12);
+            this.txtSeparator.Name = "txtSeparator";
+            this.txtSeparator.Size = new System.Drawing.Size(110, 20);
+            this.txtSeparator.TabIndex = 5;
+            this.txtSeparator.Text = ",";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(589, 339);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtSeparator);
+            this.Controls.Add(this.txtOutput);
+            this.Controls.Add(this.txtRaw);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtItemPerRow);
+            this.Controls.Add(this.button1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "Combiner";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox txtItemPerRow;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.RichTextBox txtRaw;
+        private System.Windows.Forms.RichTextBox txtOutput;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtSeparator;
+    }
+}
+
---
+++ Combiner/Form1.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Combiner
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string[] strs = txtRaw.Text.Split(new char[] { '\n' });
+            int itemPerRow = 0;
+            try
+            {
+                itemPerRow = int.Parse(txtItemPerRow.Text);
+            }
+            catch
+            {
+            }
+            if (itemPerRow == 0) { itemPerRow = 10; txtItemPerRow.Text = "10"; }
+            if (string.IsNullOrEmpty(txtSeparator.Text)) txtSeparator.Text = ",";
+            int index = 0;
+            StringBuilder sb = new StringBuilder();
+
+            foreach (string str in strs)
+            {
+                if (index != 0 && (index % itemPerRow) == 0)
+                {
+                    sb.Append("\n");
+                }
+                sb.Append(str.Trim() + txtSeparator.Text + "\t");
+                index++;
+            }
+            string output = sb.ToString().Trim();
+            if (output.EndsWith(txtSeparator.Text)) output = output.Substring(0, output.Length - txtSeparator.Text.Length);
+            txtOutput.Text = output;
+        }
+    }
+}
